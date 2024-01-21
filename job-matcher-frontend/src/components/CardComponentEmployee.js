b114da9133080c5201116844cc5a9ec2a458fc0b
Samridhi Pramanik <samridhipramanik@gmail.com>
2024-01-21 04:37:31 -0800

    card component fix WIP- resume

--- job-matcher-frontend/src/components/CardComponentEmployee.js
+++ job-matcher-frontend/src/components/CardComponentEmployee.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import logo from '../images/logo.jpeg';
 import '../css/CardComponent.css';
+
 
 function CardComponentEmployee({ applicant }) {
   return (
-    <Card style={{ width: '100%' }}>
-      {/* <Card.Img variant="top" src={user.resume} style={{ maxWidth: '100%', height: 'auto' }} /> */}
-      <Card.Body>
-        <br/>
-        <Card.Title className="company-title"><strong>Name: </strong>{applicant.first_name} {applicant.last_name}</Card.Title>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item className="list-item">
-          <strong>Email: </strong> {applicant.email}
-        </ListGroup.Item>
-        <ListGroup.Item className="list-item">
-          <strong>Job Title: </strong> {applicant.title}
-        </ListGroup.Item>
-        <ListGroup.Item className="list-item">
-          <strong>Job Id: </strong> {applicant.job_id}
-        </ListGroup.Item>
-      </ListGroup>
-    </Card>
+    <Card style={{ width: '80%', textAlign: 'center' }}>
+  {/* <Card.Img variant="top" src={user.resume} style={{ maxWidth: '100%', height: 'auto' }} /> */}
+  {/* <Card.Img variant="top" src={job.image_url} style={{ maxWidth: '100px', height: 'auto' ,display: 'block', margin: '0 auto',}} /> */}
+  <Card.Body className="d-flex flex-column align-items-center">
+    <br/>
+    <Card.Title className="company-title"><strong>Name: </strong>{applicant.first_name} {applicant.last_name}</Card.Title>
+    {/* <Card.Link
+            href={`data:${applicant.resume.contentType};base64,${applicant.resume.data.toString('base64')}`}
+            target="_blank"
+          >
+            View Resume
+          </Card.Link> */}
+  </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroup.Item className="list-item">
+      <strong>Email: </strong> {applicant.email}
+    </ListGroup.Item>
+    <ListGroup.Item className="list-item">
+      <strong>Job Title: </strong> {applicant.title}
+    </ListGroup.Item>
+    <ListGroup.Item className="list-item">
+      <strong>Job Id: </strong> {applicant.job_id}
+    </ListGroup.Item>
+  </ListGroup>
+</Card>
+
   );
 }
 
--- job-matcher-frontend/src/components/HomePageEmployee.js
+++ job-matcher-frontend/src/components/HomePageEmployee.js
@@ -44,35 +44,27 @@ const HomePageEmployee = () => {
   // Get the current job data
     const currentApplicant = applicants[currentApplicantIndex];
 
-    // try {
-    //   // Send a POST request to save the job
-    //   // console.log(currentJob.id,currentJob.title,currentJob.company,currentJob.min_salary_usd,currentJob.max_salary_usd);
-    //   // console.log(currentJob.location_iso, currentJob.job_type, currentJob.degree_required,  currentJob.url, currentJob.technologies);
-    //   await axios.post('http://localhost:3000/savedJobs', {
-    //     user_id: currentApplicant.id,
-    //     first_name: currentApplicant.first_name,
-    //     last_name: currentApplicant.last_name,
-    //     email: currentApplicant.email,
-    //     job_id: currentApplicant.job_id,
-    //     title: currentApplicant.title,
-    //     compnay: currentApplicant.company,
-    //   }, {
-    //     params: {
-    //       user_id,
-    //     },
-    //   });
-
-    // } catch (error) {
-    //   console.error('Error saving job:', error.message);
-    //   // Handle the error, show a notification, or provide feedback to the user.
-    // }
+    try {
+      // Send a POST request to matchedJobs
+      await axios.post('http://localhost:3000/matchedJobs', {
+        job_id: currentApplicant.job_id,
+        title: currentApplicant.title,
+        compnay: currentApplicant.company,
+      }, {
+        params: {
+          user_id,
+        },
+      });
+    } catch (error) {
+      console.error('Error saving job:', error.message);
+    }
   };
 
   return (
     <div className="h">
       <NavBar />
-      <div><h2 style={{ fontFamily: 'Josefin Sans' }}>Welcome to Career Crush!</h2></div>
-      <div className='home-page'>
+      <div style={{ textAlign: 'center', paddingTop: '40px' }}><h2 style={{ fontFamily: 'Josefin Sans'}}>Get Matched to most desired job seekers!</h2></div>
+      <div style={{padding: '100px', marginTop: '50px'}}>
         {currentApplicantIndex < applicants.length ? (
           <div className='swipe'>
             <div className="buttons-container">
